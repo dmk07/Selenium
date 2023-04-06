66a5a7ecab635f66dc3559d6919e3d78336a578a
dmk61 <dmk61@Dipak-Kadam>
2023-04-06 10:06:10 +0530

    FirstName LastName Email JobTitle Company Employees Phone Country State
    done

--- Selenium/src/com/register/SalesforceSignUp.java
+++ Selenium/src/com/register/SalesforceSignUp.java
@@ -2,8 +2,9 @@ package com.register;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class SalesforceSignUp {
 
@@ -23,20 +24,76 @@ public class SalesforceSignUp {
 		Thread.sleep(1000);
 		driver.findElement(By.name("UserLastName")).sendKeys("Kadam");
 		Thread.sleep(2000);
-		
 		driver.findElement(By.name("UserEmail")).sendKeys("dipak@gmail.com");	
 
 	}
 	
-	public void selectDropDown()
+	public void selectDropDown() throws InterruptedException
 	{
 		
+		WebElement we=driver.findElement(By.name("UserTitle"));
+		Select select=new Select(we);
+		select.selectByVisibleText("Sales Manager");
+		Thread.sleep(3000);
+		select.selectByIndex(1);
+		Thread.sleep(3000);		
 	}
+	
+	public void selectJobTitle() throws InterruptedException
+	{
+		WebElement we=driver.findElement(By.name("UserTitle"));
+		Select select =new Select(we);
+		select.selectByVisibleText("Sales Manager");
+		Thread.sleep(1000);
+		select.selectByIndex(1);
+		Thread.sleep(2000);
+		driver.findElement(By.name("CompanyName")).sendKeys("Infotech");
+		
+	}
+	public void selectEmployees() throws InterruptedException
+	{
+		WebElement we=driver.findElement(By.name("CompanyEmployees"));
+		Select select =new Select(we);
+		select.selectByVisibleText("1 - 15 employees");
+		Thread.sleep(1000);
+		select.selectByIndex(1);
+		Thread.sleep(2000);
+		driver.findElement(By.name("UserPhone")).sendKeys("7057807655");
+	}
+	public void selectCountry() throws InterruptedException
+	{
+		WebElement we=driver.findElement(By.name("CompanyCountry"));
+		Select select =new Select(we);
+		select.selectByVisibleText("United States");
+		Thread.sleep(1000);
+		select.selectByIndex(1);
+		Thread.sleep(2000);
+		
+	}
+	public void selectState() throws InterruptedException
+	{
+		WebElement we=driver.findElement(By.name("CompanyState"));
+		Select select =new Select(we);
+		select.selectByVisibleText("Alabama");
+		Thread.sleep(1000);
+		select.selectByIndex(1);
+		Thread.sleep(2000);
+		
+	}
+	
+	
+	
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		SalesforceSignUp obj=new SalesforceSignUp();
 		obj.launchAUT();
+		obj.selectDropDown();
+		obj.selectJobTitle();
+		obj.selectEmployees();
+		obj.selectCountry();
+		obj.selectState();
+		
 		
 		
 	}
